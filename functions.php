d44d2d0efd6b3e832c4273a488d01123e9032b86
abaicus <baicusandrei@me.com>
2015-12-15 17:11:44 +0200

    !!!Added TGM plugin activator.

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function roda_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => __( 'Primary Menu', 'roda' ),
-		'social' => __( 'Social', 'roda' ),		
+		'social' => __( 'Social', 'roda' ),
 	) );
 
 	/*
@@ -101,7 +101,7 @@ function roda_scripts() {
 
 	$fonts = get_theme_mod('roda_fonts');
 	if( $fonts ) {
-		wp_enqueue_style( 'roda-fonts', '//fonts.googleapis.com/css?family='. esc_attr($fonts) );	
+		wp_enqueue_style( 'roda-fonts', '//fonts.googleapis.com/css?family='. esc_attr($fonts) );
 	} else {
 		wp_enqueue_style( 'roda-fonts', '//fonts.googleapis.com/css?family=Fira+Sans:400,700,400italic,700italic');
 	}
@@ -110,9 +110,9 @@ function roda_scripts() {
 
 	wp_enqueue_script( 'roda-bootstrap-js', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array('jquery'), true );
 
-	wp_enqueue_script( 'roda-fitvids', get_template_directory_uri() . '/js/jquery.fitvids.js', array('jquery'), true );	
+	wp_enqueue_script( 'roda-fitvids', get_template_directory_uri() . '/js/jquery.fitvids.js', array('jquery'), true );
 
-	wp_enqueue_script( 'roda-slicknav', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array(), true );	
+	wp_enqueue_script( 'roda-slicknav', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array(), true );
 
 	wp_enqueue_script( 'roda-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), true );
 
@@ -128,7 +128,7 @@ add_action( 'wp_enqueue_scripts', 'roda_scripts' );
  * Change the excerpt length
  */
 function roda_excerpt_length( $length ) {
-	
+
 	$excerpt = get_theme_mod('exc_lenght', '55');
 	return $excerpt;
 
@@ -143,7 +143,7 @@ function roda_html5shiv() {
     echo '<script src="' . esc_url( get_template_directory_uri() . '/js/html5shiv.js' ) . '"></script>' . "\n";
     echo '<![endif]-->' . "\n";
 }
-add_action( 'wp_head', 'roda_html5shiv' ); 
+add_action( 'wp_head', 'roda_html5shiv' );
 
 /**
  * Custom template tags for this theme.
@@ -180,3 +180,67 @@ require get_template_directory() . '/inc/svg.php';
  * Slider
  */
 require get_template_directory() . '/inc/slider.php';
+
+
+/**
+ *TGM Plugin activation.
+ */
+require_once get_template_directory() . '/class-tgm-plugin-activation.php';
+
+/**
+ * TGMPA register
+ */
+function roda_register_required_plugins() {
+		$plugins = array(
+			array(
+				'name'      => 'WP Product Reviews',
+				'slug'      => 'wp-product-reviews',
+				'required'  => false,
+			),
+
+			array(
+				'name'      => 'Intergeo Maps - Google Maps Plugin',
+				'slug'      => 'intergeo-maps',
+				'required'  => false
+			),
+
+			array(
+				'name'     => 'Pirate Forms',
+				'slug' 	   => 'pirate-forms',
+				'required' => false
+			));
+
+	$config = array(
+        'default_path' => '',
+        'menu'         => 'tgmpa-install-plugins',
+        'has_notices'  => true,
+        'dismissable'  => true,
+        'dismiss_msg'  => '',
+        'is_automatic' => false,
+        'message'      => '',
+        'strings'      => array(
+            'page_title'                      => esc_html__( 'Install Required Plugins', 'roda' ),
+            'menu_title'                      => esc_html__( 'Install Plugins', 'roda' ),
+            'installing'                      => esc_html__( 'Installing Plugin: %s', 'roda' ),
+            'oops'                            => esc_html__( 'Something went wrong with the plugin API.', 'roda' ),
+            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'roda' ),
+            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'roda' ),
+            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'roda' ),
+            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'roda' ),
+            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'roda' ),
+            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'roda' ),
+            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'roda' ),
+            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'roda' ),
+            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'roda' ),
+            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'roda' ),
+            'return'                          => esc_html__( 'Return to Required Plugins Installer', 'roda' ),
+            'plugin_activated'                => esc_html__( 'Plugin activated successfully.', 'roda' ),
+            'complete'                        => esc_html__( 'All plugins installed and activated successfully. %s', 'roda' ),
+            'nag_type'                        => 'updated'
+        )
+    );
+
+	tgmpa( $plugins, $config );
+
+}
+add_action( 'tgmpa_register', 'roda_register_required_plugins' );
