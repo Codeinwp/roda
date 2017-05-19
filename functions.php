e25ad649cd7207bcdb4c70f9962c516f8bc6d8fe
Poonam <poonam@themeisle.com>
2017-05-19 14:41:13 +0530

    Fixed #6

--- functions.php
+++ functions.php
@@ -193,8 +193,8 @@ require_once get_template_directory() . '/class-tgm-plugin-activation.php';
 function roda_register_required_plugins() {
 		$plugins = array(
 			array(
-				'name'      => 'WP Product Reviews',
-				'slug'      => 'wp-product-reviews',
+				'name'      => 'WP Product Review',
+				'slug'      => 'wp-product-review',
 				'required'  => false,
 			),
 
